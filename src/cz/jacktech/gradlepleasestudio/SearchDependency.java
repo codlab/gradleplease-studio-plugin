dfd42819535f7f55ea8d4fdab182a4f6215c65c8
jacktech24 <jacktech24@gmail.com>
2015-06-27 23:29:57 +0200

    init

---
+++ src/cz/jacktech/gradlepleasestudio/SearchDependency.java
@@ -0,0 +1,15 @@
+package cz.jacktech.gradlepleasestudio;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by toor on 27.6.15.
+ */
+public class SearchDependency extends AnAction {
+
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+
+}
